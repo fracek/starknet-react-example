b6a66ce65b33675e6b35746591609c867998b03d
Francesco Ceccon <francesco@ceccon.me>
2021-12-28 16:57:24 +0000

    fix: starknet import

--- src/providers/TransactionsProvider/model.ts
+++ src/providers/TransactionsProvider/model.ts
@@ -1,7 +1,7 @@
-import { AddTransactionResponse, Status, TxStatus } from "starknet";
+import { AddTransactionResponse, Status, TransactionStatus } from "starknet";
 
 export interface StoredTransaction {
-  code: TxStatus | Status;
+  code: Status | TransactionStatus;
   hash: string;
   address?: string;
   lastChecked: string;
